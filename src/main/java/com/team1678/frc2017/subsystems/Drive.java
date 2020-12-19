2b73d7b6038882145bd2338b5909cb7a38f5daf8
Tom <treez.zhang@gmail.com>
2020-12-18 21:23:16 -0800

    delete REVColour and cleanup

--- src/main/java/com/team1678/frc2017/subsystems/Drive.java
+++ src/main/java/com/team1678/frc2017/subsystems/Drive.java
@@ -567,9 +567,8 @@ public class Drive extends Subsystem {
         double deltaRightTicks = ((mPeriodicIO.right_position_ticks - prevRightTicks) / (DRIVE_ENCODER_PPR)) * Math.PI;
         mPeriodicIO.right_distance += deltaRightTicks * Constants.kDriveWheelDiameterInches;
 
-        //Deprecated
-        //mPeriodicIO.left_current = mLeftMaster.getOutputCurrent();
-        //mPeriodicIO.right_current = mRightMaster.getOutputCurrent();
+        mPeriodicIO.left_current = mLeftMaster.getOutputCurrent();
+        mPeriodicIO.right_current = mRightMaster.getOutputCurrent();
 
         // System.out.println("control state: " + mDriveControlState + ", left: " + mPeriodicIO.left_demand + ", right: " + mPeriodicIO.right_demand);
     }
